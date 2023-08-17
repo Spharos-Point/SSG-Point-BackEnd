cacbdc25bad8d8839cc1f268a55246134512bc1a
ycm0926 <yangyang09261@naver.com>
2023-08-17 20:49:15 +0900

    "user update v1"

--- src/main/java/com/spharos/pointapp/user/infrastructure/UserRepository.java
+++ src/main/java/com/spharos/pointapp/user/infrastructure/UserRepository.java
@@ -2,7 +2,11 @@ package com.spharos.pointapp.user.infrastructure;
 
 import com.spharos.pointapp.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByLoginId(String loginId);
-    User findByUUID(String UUID);
+    Optional<User> findByUuid(String uuid);
+
 }
