18bf560c57bf5edf889a60460ea833407b6eb9ab
hibaek99 <tjdwjs9903@naver.com>
2023-09-03 21:59:37 +0900

    사용자 참여 이벤트, 사용자 보유 쿠폰 완성

--- src/main/java/com/spharos/pointapp/config/common/BaseTimeEntity.java
+++ src/main/java/com/spharos/pointapp/config/common/BaseTimeEntity.java
@@ -1,12 +1,17 @@
+package com.spharos.pointapp.config.common;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.EntityListeners;
+import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 //공통 매핑 정보가 필요할 때, 부모 클래스에 선언하고 속성만 상속 받아서 사용하고 싶을 때 사용
+
+@Getter
 @MappedSuperclass
 //엔티티의 생성, 수정, 삭제 등과 같은 중요한 이벤트가 발생할 때 이를 감지하고 특정 로직을 실행할 수 있도록 해줍니다.
 @EntityListeners(AuditingEntityListener.class)
